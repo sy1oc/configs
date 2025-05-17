3464c2d3c7268d90e5ded7a020c3d9e505db0f44
sy1oc <zachlocamon@outlook.com>
2025-05-17 23:20:56 +0800

    +(nvim:mason-lspconfig);+[.xinitrc](export_proxy)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,13 +6,13 @@ require('configs.cat')
 
 require('lualine').setup({})
 require('nvim-autopairs').setup()
-require('mason').setup()
+require('mason').setup {
+}
 require('mason-lspconfig').setup {
-    automatic_enable = false,
     ensure_installed = {
         'rust_analyzer',
         'lua_ls',
         'clangd',
         'texlab',
-    }
+    },
 }
