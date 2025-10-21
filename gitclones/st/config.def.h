06b84d9f0aa71bd7adee5f3166f2f82ce1246781
sy1oc <2816135275@qq.com>
2025-10-21 11:13:48 +0800

    add lsp fmt( fmt when save need to be change)

--- gitclones/st/config.def.h
+++ gitclones/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:size=12";
+static char *font = "JetBrainsMono Nerd Font:size=15";
 static int borderpx = 2;
 
 /*
